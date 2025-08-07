a4ed5c6c2bb21dad597b63e17228408bcb37a744
TianaPM <tiana.pm@outlook.com>
2025-08-06 19:26:32 -0600

    Add white triangle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+import player
 
 def main():
 
@@ -9,15 +10,23 @@ def main():
 
     pygame.init()
 
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    p1 = player.Player(SCREEN_WIDTH/2, SCREEN_WIDTH/2)
+
     while(True):
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        screen.fill("black")
-        pygame.display.flip()
+        screen.fill((0,0,0))
+        p1.draw(screen)
+        pygame.display.flip()    
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
